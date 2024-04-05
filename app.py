9370ee13ee7b9f0b948d613ae834b7d932c21499
douaa <douaa.BENHILA@etu.univ-amu.fr>
2024-04-05 18:06:53 +0200

    reussir l'authentification on créant la méthode  def login() et gérer l'affichage quand la connexin est réussi

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask , render_template , request, redirect, session # type: ignore #importer flask
+from flask import Flask , render_template , request, redirect, session  #importation
 import sqlite3 
 
 app = Flask(__name__) # créer une instance du flask
+app.secret_key = 'benhilaDouaa'# Clé secrète pour signer la session j'ai remarqué que si je mais pas de clé secrete ca marche pas et ca m'affiche une page qui me demande de le créer 
+
 # créer table user
 def creer_table():
 #créer une connexion avec ma base de données 
@@ -37,17 +39,51 @@ def creer_table():
     print(res.fetchall())
 # fermer la connexion avec la base de données
     conn.close()
-
-
-
 creer_table()
 
 
+# implemnter la méthode d'authentification (logique dans solution minimale)
+def login(user_id, password):
+    conn = sqlite3.connect('database.db')  #crée une connexion à la base de données
+    cur = conn.cursor() #créer objet curseur qui va nous permettre d'executer des requetes sql
+    cur.execute("SELECT id FROM USER WHERE user_id = ? AND password = ?", (user_id, password)) # requette sql basique comme vu au s3
+    user = cur.fetchone() # récupére le résultat de la requete
+    conn.close() # on ferme la connexion à la base de données
+    if user:
+        return user[0]  #si la connexion est réussi , je retourne la clé id associé à l'utilisateur
+    else:
+        return -1  # Retourne -1 , sinon 
 
-#définir les routes et les vues 
-@app.route('/')
-def home():
-    return render_template('home.html')
+# defini les routes 
+@app.route('/', methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        #récupère les valeurs de formulaire
+        user_id = request.form['user_id']
+        password = request.form['password']
+        # retourne l'id s'il existe dans une base de données
+        user_id_db = login(user_id, password)
+        # si l id est different de -1 du coup la connexion est réussi 
+        if user_id_db != -1:
+            session['user_id'] = user_id_db 
+            return redirect('/dashboard')
+        else:
+            error = 'Identifiants incorrects. Veuillez réessayer.'
+            return render_template('home.html', error=error)
+    else:
+        return render_template('home.html')
+
+
+#page temporaire 
+# Page du tableau de bord
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' in session:
+        # Si l'utilisateur est connecté, renvoyer le tableau de bord
+        return render_template('dashboard.html')
+    else:
+        # Sinon, rediriger vers la page de connexion
+        return redirect('/login')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
