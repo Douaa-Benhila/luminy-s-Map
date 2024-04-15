ae0ccdaa6c8b8f40e3c269e83e2394c06b4b3fe5
douaa <douaa.BENHILA@etu.univ-amu.fr>
2024-04-15 14:14:36 +0200

    implementer une méthode pour se connecter plus ajouter des routes

--- app.py
+++ app.py
@@ -38,6 +38,32 @@ def creer_table():
 creer_table()
 
 
+# implemnter la méthode d'authentification (logique dans solution minimale)
+def login(username, password):
+    conn = sqlite3.connect('database.db')  #crée une connexion à la base de données
+    cur = conn.cursor() #créer objet curseur qui va nous permettre d'executer des requetes sql
+    cur.execute("SELECT id FROM USER WHERE username = ? AND password = ?", (username, password)) # requette sql basique comme vu au s3
+    user = cur.fetchone() # récupére le résultat de la requete
+    conn.close() # on ferme la connexion à la base de données
+    if user:
+        return user[0]  #si la connexion est réussi , je retourne la clé id associé à l'utilisateur
+    else:
+        return None  # Retourne un message  , sinon (changer ma solution minimale)
+
+
+
+
+# Page du map
+@app.route('/carte.html')
+def carte():
+    if 'user_id' in session:
+        # Si l'utilisateur est connecté, renvoyer la map
+        return render_template('carte.html')
+    else:
+        # Sinon, rediriger vers la page de connexion
+        return redirect('/login')
+
+
 #définir les routes
 # route pour la page d'accueil
 @app.route('/')
@@ -45,9 +71,28 @@ def home():
     return render_template('home.html')
 
 # route pour la page de connexion
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    error_message = None  # Initialisez la variable d'erreur à None
+    if request.method == 'POST':
+        # Récupère les valeurs de formulaire
+        username = request.form['username']
+        password = request.form['password']
+        # Retourne l'id s'il existe dans une base de données
+        user_id_db = login(username, password)
+        # Si l'id existe, connectez l'utilisateur
+        if user_id_db is not None:
+            session['user_id'] = user_id_db  # Utilisez la bonne clé pour stocker l'ID de l'utilisateur
+            return redirect('/carte.html')
+        else:
+            error_message = 'Mauvais identifiant / mot de passe.'
+    else:
+        # Redirection vers la page de connexion si la méthode de requête est GET
+        return redirect('/login')
+           
+    return render_template('login.html', error_message=error_message)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
